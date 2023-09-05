e8279de6e18e1ad8abe23978851854db458ea6b6
blusine <blusine@hotmail.com>
2023-09-05 06:11:09 +0000

    file upload

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ from werkzeug.datastructures import FileStorage
 #from flask_uploads import UploadSet, configure_uploads, DOCUMENTS
 
 app = Flask(__name__)
-#app.config['UPLOAD_FOLDER'] = 'uploads'
-#app.secret_key = 'some_secret_key'
-app.config['SECRET_KEY'] = 'YourSecretKey'
-app.config['UPLOAD_FOLDER'] = 'uploads'
+UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'docx'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'supersecretkey'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -32,8 +32,36 @@ class NLPForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            # Process the document here
+            return redirect(url_for('results', filename=filename))
+    return render_template('index.html')
 
+@app.route('/results/<filename>')
+def results(filename):
+    # You can use TextBlob or other NLP libraries to process the file here
+    # and show the results.
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    with open(file_path, 'r') as f:
+        content = f.read()
+        blob = TextBlob(content)
+        # Do more with the content, analyze, etc.
+    return render_template('results.html', content=content, analysis=blob.sentences)
+  
 if __name__ == '__main__':
   #app.run(debug=True)
   app.run(host='0.0.0.0', debug=True)
